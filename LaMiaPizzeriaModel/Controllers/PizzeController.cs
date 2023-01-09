de8751cbcb72434b19106a882c7b157cd0bcd26f
adelademarku <adelademarku@email.com>
2023-01-09 15:24:46 +0100

    Aggiungere i file di progetto.

---
+++ LaMiaPizzeriaModel/Controllers/PizzeController.cs
@@ -0,0 +1,39 @@
+﻿using LaMiaPizzeriaModel.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using LaMiaPizzeriaModel.Models;
+namespace LaMiaPizzeriaModel.Controllers
+{
+    public class PizzeController : Controller
+    {
+        public IActionResult PizzeIndex()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            List<Pizza> listaDeiPost = DataPizza.GetPizze();
+
+            return View("Index", listaDeiPost);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> listaDellePizze = DataPizza.GetPizze();
+
+            foreach (Pizza pizza in listaDellePizze)
+            {
+                if (pizza.Id == id)
+                {
+                    return View(pizza);
+                }
+            }
+
+            return NotFound("Il post con l'id cercato non esiste!");
+        }
+
+
+
+    }
+}
---
+++ LaMiaPizzeriaModel/Utils/DataPizza.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using LaMiaPizzeriaModel.Models;
+
+namespace LaMiaPizzeriaModel.Utils
+{
+    public static class DataPizza
+    {
+        private static List<Pizza> pizze;
+
+        public static List<Pizza> GetPizze()
+        {
+
+            if (pizze != null)
+            {
+                return pizze;
+            }
+
+            pizze = new List<Pizza>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Pizza postGenerato = new Pizza(i, "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://picsum.photos/id/" + i + "/300");
+                pizze.Add(postGenerato);
+            }
+            return pizze;
+
+        }
+    }
+}
